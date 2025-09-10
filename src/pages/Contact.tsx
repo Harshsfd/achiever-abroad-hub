f1a897ad11f1910025e7d1400caa64b4e85eb5d3
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-10 14:11:04 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/hooks/use-toast";
-import { sendContactForm } from "@/api";
+
 import {
   Phone,
   Mail,
