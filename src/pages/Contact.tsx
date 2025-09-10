c29828372cf66e8ac94e676dfc898469805d06eb
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-10 17:15:44 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/hooks/use-toast";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 import {
   Phone,
